3e3f8ec05370ceb4e80c845b1892bc95ebdd628d
Enrico <enrico@cryms.com>
2022-03-06 20:48:52 +0100

    Project init

---
+++ src/main/java/com/tarma/kisabackendauth/KisaBackendAuthApplication.java
@@ -0,0 +1,13 @@
+package com.tarma.kisabackendauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KisaBackendAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KisaBackendAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarma/kisabackendauth/KisaBackendAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarma.kisabackendauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KisaBackendAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
