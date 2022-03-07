ab03dc5e33787a168caf195eb63bef1cca9bae49
Enrico <enrico@cryms.com>
2022-03-07 12:21:50 +0100

    Disable cors for localhost:3000

---
+++ src/main/java/com/tarma/kisabackendauth/security/ApplicationSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.tarma.kisabackendauth.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().authorizeHttpRequests().anyRequest().authenticated();
+    }
+}
--- src/main/java/com/tarma/kisabackendauth/student/StudentController.java
+++ src/main/java/com/tarma/kisabackendauth/student/StudentController.java
@@ -1,14 +1,12 @@
 package com.tarma.kisabackendauth.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("api/students")
 public class StudentController {
 
@@ -16,7 +14,6 @@ public class StudentController {
             Student.builder().studentId(1).studentName("James Bond").build(),
             Student.builder().studentId(2).studentName("Joh Doe").build(),
             Student.builder().studentId(3).studentName("Marco Rossi").build());
-
     @GetMapping("/{studentId}")
     public Student getStudent(@PathVariable("studentId") long studentId) {
         return STUDENTS.stream().filter(s -> s.getStudentId() == studentId).findFirst()
