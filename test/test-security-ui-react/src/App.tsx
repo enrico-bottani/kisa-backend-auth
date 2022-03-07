d5e5b7397ea6ff88de426a25c9bc3d795eea4e1b
Enrico <enrico@cryms.com>
2022-03-07 12:22:12 +0100

    Disable cors for localhost:3000

---
+++ test/test-security-ui-react/src/App.tsx
@@ -0,0 +1,36 @@
+import React, {useEffect} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+    useEffect(() => {
+        let request = "http://localhost:8080/api/students/1";
+        fetch(request,{mode:"cors",method:"GET"}).then(
+            response=>{
+                return response.json();
+            }
+        ).then(
+            body=>{
+                console.log(body)
+            }
+        ).catch(
+            e=>{
+                console.error("Error on request: "+request)
+            }
+        )
+    }, [])
+
+    return (
+        <div>
+            <header>
+                <h1>Header</h1>
+            </header>
+            <main>
+
+            </main>
+        </div>
+    );
+}
+
+export default App;
